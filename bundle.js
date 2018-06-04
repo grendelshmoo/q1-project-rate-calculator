1b163286230c12a497adf640eb1da14ba899b61d
craig <grendel@shmoo.com>
2018-06-04 11:31:10 -0700

    Finished templages for general & summary Panes, finished render functions for both.

--- bundle.js
+++ bundle.js
@@ -1,3 +1,26 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+// const data = require('./data')
+const templates = require('./templates')
+
+
+//Render the Summary Column
+function renderSummary () {
+  document.getElementById('summaryPane').innerHTML = templates.summaryTemplate()
+}
+renderSummary()
+
+
+//Render the General Pane
+function renderGeneral() {
+  document.getElementById('generalPane').innerHTML = templates.generalTemplate()
+}
+renderGeneral()
+
+//On Input Event Listener for Sales Price
+
+//On Input Event Listener for Loan Amount
+
+},{"./templates":2}],2:[function(require,module,exports){
 // function generalTemplate (general) {
 //   return `
 //   <tr>
@@ -157,3 +180,5 @@ module.exports = {
   summaryTemplate,
   generalTemplate
 }
+
+},{}]},{},[1]);
